1cd9ccbc5593fadebee0570363f2bc5bee2e777b
idiocy-personified <136112177+idiocy-personified@users.noreply.github.com>
2026-01-28 19:01:25 +0000

    Basic movement
    Basic WASD and jumping movement

---
+++ Assets/Scripts/PLayer Controls/PlayerCamera.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    //this is the players camera controls and stuff
+
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //gets the moust input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90);
+
+
+        //rotate camera orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PLayer Controls/PlayerMovement.cs
@@ -0,0 +1,134 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //handles all the player movement stuff
+
+    //being lazy and making everything public for now so I can edit values in the editor, headers to make it more readable in editor
+
+    [Header("Movement variables")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float maxVelocity;
+
+    [Header("Jumping Variables")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public float airDrag;
+    bool canJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground check Variables")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody playerRB;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.freezeRotation = true;
+        canJump = true;
+       
+    }
+
+    private void Update()
+    {
+        //check if the player is on the ground
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        //check players input every frame
+        PlayerInput();
+        SpeedCheck();
+
+        //handle drag
+        if (isGrounded == true )
+        {
+            playerRB.linearDamping = groundDrag;
+        }
+        else
+        {
+            playerRB.linearDamping = airDrag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player 60 times a second (this stops movement being linked to frame rate)
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when the player can jump
+        if(Input.GetKey(jumpKey) && canJump && isGrounded)
+        {
+            canJump = false;
+            Jump();
+
+            //lets you hold down space to Bhop
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        //Move direction math
+
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        if (isGrounded == true)
+        {
+            playerRB.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (isGrounded == false)
+        {
+            playerRB.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+
+    }
+
+
+    private void SpeedCheck()
+    {
+        //calculates players velocity can use this to scale healing later
+        Vector3 flatvel = new Vector3(playerRB.linearVelocity.x, 0f, playerRB.linearVelocity.y);
+
+        //if our cu7rrent velocity is greater than our maximum velocity the speed is brought down too the max velocity
+
+        //if(flatvel.magnitude > maxVelocity)
+        //{
+        //    Vector3 limitedVel = flatvel.normalized * maxVelocity;
+        //    playerRB.linearVelocity = new Vector3(limitedVel.x, playerRB.linearVelocity.y, limitedVel.z);
+        //}
+    }
+
+
+    private void Jump()
+    {
+        //reset vertical velocity when jump is called
+        playerRB.linearVelocity = new Vector3(playerRB.linearVelocity.x, 0f, playerRB.linearVelocity.z);
+
+        playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        canJump = true;
+    }
+}
